d5980abf4638d23effb9d1c477cb1ad3b8935e1d
Stabilo <zteo015@e.ntu.edu.sg>
2022-08-09 18:56:15 +0800

    added prayer circle for magic and ranged when selecting prayer

--- script.js
+++ script.js
@@ -25,10 +25,29 @@ let deathCount = 0;
 const changeInventoryItem = () => {
   $("#inventory").css("background-image", "url(images/inventory.JPG)");
 };
-
 // function to change inventory screen to prayer
 const changeInventoryPrayer = () => {
   $("#inventory").css("background-image", "url(images/prayers.JPG)");
+};
+// function to append css of a circle to magic
+const magicPrayerCircle = () => {
+  $("#pray-magic").css("background-color", "lightyellow");
+  $("#pray-magic").css("opacity", "0.3");
+  $("#pray-magic").css("border-radius", "50%");
+};
+// function to append css of a circle to ranged
+const rangePrayerCircle = () => {
+  $("#pray-ranged").css("background-color", "lightyellow");
+  $("#pray-ranged").css("opacity", "0.3");
+  $("#pray-ranged").css("border-radius", "50%");
+};
+// function to remove css of magic circle
+const removeMagicPrayerCircle = () => {
+  $("#pray-magic").css("opacity", "0");
+};
+// function to remove css of ranged circle
+const removeRangePrayerCircle = () => {
+  $("#pray-ranged").css("opacity", "0");
 };
 
 $(() => {
@@ -68,7 +87,6 @@ $(() => {
     $("#main-invent-item")[0].style.display = "none";
     $("#main-invent-pray")[0].style.display = "";
     changeInventoryPrayer();
-
   });
   // for loop for each inventory item
   // depends on whether screen is prayer screen or inventory screen
@@ -80,45 +98,65 @@ $(() => {
     });
   }
 
+  // onclick pray-magic, call funtion magicPrayerCircle
+  $("#pray-magic").on("click", () => {
+    magicPrayerCircle();
+    removeRangePrayerCircle();
+    selectedPrayer = "magic";
+  });
+
+  // onclick pray-range, call funtion rangePrayerCircle
+  $("#pray-ranged").on("click", () => {
+    rangePrayerCircle();
+    removeMagicPrayerCircle();
+    selectedPrayer = "ranged";
+  });
+
   // If prayer on magic
   // Click magic becomes none, click ranged becomes ranged
   // white is none, red is magic, green is ranged
-  if ($(`#prayer`)[0].style.background == "red") {
-    $(`#pray-magic`).on("click", () => {
-      let selectedPrayer = "none";
-      $(`#prayer`)[0].style.background = "white";
-      console.log("praying", selectedPrayer);
-    });
-    $(`#pray-ranged`).on("click", () => {
-      let selectedPrayer = "ranged";
-      $(`#prayer`)[0].style.background = "green";
-      console.log("praying", selectedPrayer);
-    });
-  }
-  if ($(`#prayer`)[0].style.background == "green") {
-    $(`#pray-ranged`).on("click", () => {
-      let selectedPrayer = "none";
-      console.log("praying", selectedPrayer);
-      $(`#prayer`)[0].style.background = "white";
-    });
-    $(`#pray-magic`).on("click", () => {
-      let selectedPrayer = "magic";
-      console.log("praying", selectedPrayer);
-      $(`#prayer`)[0].style.background = "red";
-    });
-  }
-  if ($(`#prayer`)[0].style.background == "white") {
-    $(`#pray-ranged`).on("click", () => {
-      let selectedPrayer = "ranged";
-      console.log("praying", selectedPrayer);
-      $(`#prayer`)[0].style.background = "green";
-    });
-    $(`#pray-magic`).on("click", () => {
-      let selectedPrayer = "magic";
-      console.log("praying", selectedPrayer);
-      $(`#prayer`)[0].style.background = "red";
-    });
-  }
+  // if ($(`#prayer`)[0].style.background == "red") {
+  //   $(`#pray-magic`).on("click", () => {
+  //     magicPrayerCircle();
+  //     let selectedPrayer = "none";
+  //     $(`#prayer`)[0].style.background = "white";
+  //     console.log("praying", selectedPrayer);
+  //   });
+  //   $(`#pray-ranged`).on("click", () => {
+  //     rangePrayerCircle();
+  //     let selectedPrayer = "ranged";
+  //     $(`#prayer`)[0].style.background = "green";
+  //     console.log("praying", selectedPrayer);
+  //   });
+  // }
+  // if ($(`#prayer`)[0].style.background == "green") {
+  //   $(`#pray-ranged`).on("click", () => {
+  //     rangePrayerCircle();
+  //     let selectedPrayer = "none";
+  //     console.log("praying", selectedPrayer);
+  //     $(`#prayer`)[0].style.background = "white";
+  //   });
+  //   $(`#pray-magic`).on("click", () => {
+  //     magicPrayerCircle();
+  //     let selectedPrayer = "magic";
+  //     console.log("praying", selectedPrayer);
+  //     $(`#prayer`)[0].style.background = "red";
+  //   });
+  // }
+  // if ($(`#prayer`)[0].style.background == "white") {
+  //   $(`#pray-ranged`).on("click", () => {
+  //     rangePrayerCircle();
+  //     let selectedPrayer = "ranged";
+  //     console.log("praying", selectedPrayer);
+  //     $(`#prayer`)[0].style.background = "green";
+  //   });
+  //   $(`#pray-magic`).on("click", () => {
+  //     magicPrayerCircle();
+  //     let selectedPrayer = "magic";
+  //     console.log("praying", selectedPrayer);
+  //     $(`#prayer`)[0].style.background = "red";
+  //   });
+  // }
 });
 
 // Sprites needed
